61e1282edbc59e88e99b7654d4496427fa87c3b4
ALIF <sharifahamedalif@gmail.com>
2022-10-11 13:50:09 +0600

    update all

--- script.js
+++ script.js
@@ -27,8 +27,10 @@ const ShowDisplayPhone = (data) => {
      </div>
       <img  src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">
       <div class="card-body">
-        <h5 class="card-title">${phone.phone_name}</h5>
-        <p class="card-text">${phone.slug}</p>
+        <h5 class="card-title">${phone.phone_name}</h5>        
+      </div>
+      <div class="card-footer">
+          <button onclick="LoadPhoneDetails('${(phone.slug)}')" type="button" class="btn btn-success btn-sm">Details</button>
       </div>
     </div>     
     `;
@@ -39,11 +41,45 @@ const ShowDisplayPhone = (data) => {
 }
 
 
-const showPhoneDetails = (phoneid) => {  
-  const url = `https://openapi.programming-hero.com/api/phone/=${phoneid}`;
+const LoadPhoneDetails = (id) => {  
+  const url = ` https://openapi.programming-hero.com/api/phone/${id}`;
+  // console.log(url);
   fetch(url)
   .then(response => response.json())
-  .then(data => console.log(data.data))
+  .then(data => ShowPhoneDetails(data.data))
 }
 
-showPhoneDetails();
+
+const ShowPhoneDetails = (data) => {
+  console.log(data);
+  const phoneDetails = document.getElementById('phone-details');
+  phoneDetails.textContent = '';
+  const div = document.createElement('div');
+  div.classList.add('col');
+  div.innerHTML = `
+      <div class="card">
+          <div class="card-header mb-1">
+            <h4 class="card-title">${data.brand}</h4>
+          </div>
+          <img  src="${data.image}" class="card-img-top w-50 mx-auto" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${data.name}</h5>
+            <h6>Mainfeatures</h6> 
+            <p><b>Chipset:</b> ${data.mainFeatures.chipSet}</p>
+            <p><b>Display Size:</b> ${data.mainFeatures.displaySize}</p>   
+            <p><b>Memory:</b> ${data.mainFeatures.memory}</p> 
+            <p><b>Bluetooth:</b> ${data.others.Bluetooth}</p>
+            <p><b>GPS:</b> ${data.others.GPS}</p>
+            <p><b>NFC:</b> ${data.others.NFC}</p>
+            <p><b>USB:</b> ${data.others.USB}</p>
+            <p><b>WLAN:</b> ${data.others.WLAN}</p>        
+          </div> 
+          <div class="card-footer">
+            <p><b>Release Date: </b> ${data.releaseDate}</p>
+          </div>        
+        </div>    
+      `;
+
+      phoneDetails.appendChild(div);
+
+}
